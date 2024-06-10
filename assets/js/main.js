7301beba8be447e9a4852a2362de577019c90d70
César Diego Anovich <iddiegobis@gmail.com>
2024-06-10 11:28:30 -0300

    Added Publish AMPv4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-    'event': 'pageview',
-    'page': {
-        'url': window.location.href,
-        'title': document.title,
-        'path': window.location.pathname
-    }
-});
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event': 'pageview',
+        'page': {
+            'url': window.location.href,
+            'title': document.title,
+            'path': window.location.pathname
+        }
+    });
