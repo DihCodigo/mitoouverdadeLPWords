662e8ac951ca1f8f8583ac67d895cd3e4a075310
César Diego Anovich <iddiegobis@gmail.com>
2024-06-11 15:06:34 -0300

    addev7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-console.log("Este é um script personalizado carregado em uma página AMP.");
-    
-    /*
+     /*
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
         'event': 'pageview',
