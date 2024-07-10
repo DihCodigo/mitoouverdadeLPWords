55d245b3908753229068dd4d16c1be7541ffefe3
César Diego Anovich <iddiegobis@gmail.com>
2024-07-10 17:35:12 -0300

    v2

---
+++ assets/js/teste.js
@@ -0,0 +1,96 @@
+    var googleFormats = [];
+    googleFormats = [
+[[[970, 250], [970, 90], [728, 90]],'bonsfluidos','970x250_top',[{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_970x250_top' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
+                { bidder: 'seedtag', params: {adUnitId: '29650077', publisherId: '3592-0609-01', placement: 'inBanner'}}
+            ]
+        }]],
+      [[[970, 250], [970, 90], [728, 90]], 'bonsfluidos', '970x250_middle', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_6' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
+                { bidder: 'seedtag', params: {adUnitId: '29651182', publisherId: '3592-0609-01', placement: 'inBanner'}}
+            ]
+        }]],
+   [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_right', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_1' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
+                { bidder: 'seedtag', params: {adUnitId: '21653362', publisherId: '3592-0609-01', placement: 'inArticle'}}
+            ]
+        }]],
+    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_1', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_2' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
+                { bidder: 'seedtag', params: {adUnitId: '21653364', publisherId: '3592-0609-01', placement: 'inArticle'}}
+            ]
+        }]],
+    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_2', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_3' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
+                { bidder: 'seedtag', params: {adUnitId: '21653365', publisherId: '3592-0609-01', placement: 'inArticle'}}
+            ]
+        }]],
+    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_3', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_4' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } }
+            ]
+        }]],
+    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_4', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_5' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } }
+            ]
+        }]],
+    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_5', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x600_right' } },
+                { bidder: 'appnexus', params: { placementId: 19274151 } },
+                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
+                { bidder: 'seedtag', params: {adUnitId: '29651185', publisherId: '3592-0609-01', placement: 'inBanner'}},
+            ]
+        }]],
+    [[[250, 250], [300, 600], [160, 600], [300, 250]], 'bonsfluidos', '300x600_right', [{
+            bids: [
+                { bidder: 'rubicon', params: { accountId: 10605, siteId: 55114, zoneId: 258958 } },
+                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x600_right' } },
+                { bidder: 'appnexus', params: { placementId: 19027172 } },
+                { bidder: 'teads', params: { placementId: 131494, pageId: 121195 } }
+            ]
+        }]],
+        [[[728, 90], [970 , 90]],'bonsfluidos','anchor',[{
+             bids: [
+                { bidder: 'rubicon', params: {accountId: 10605, siteId: 322594, zoneId: 1712330 } },
+                { bidder: 'appnexus', params: {placementId: 19274151} },
+                { bidder: 'pubmatic', params: {publisherId: '159456', adSlot: 'Bons_Fluidos_Anchor_970x90'} },
+                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
+                { bidder: 'seedtag', params: {adUnitId: '21653363', publisherId: '3592-0609-01', placement: 'inScreen'}}
+      
+    ]
+  }]]
+      ]
+
+      for(i = 0; i < googleFormats.length; i++){
+        console.log("-> " + googleFormats[i][2]).refresh();
+      }
+      
