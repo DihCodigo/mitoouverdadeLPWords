e6a5641c54f8509d260bb0182f9a4a124a923c39
César Diego Anovich <iddiegobis@gmail.com>
2024-07-15 10:42:01 -0300

    v1

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,96 +1,106 @@
-    var googleFormats = [];
-    googleFormats = [
-[[[970, 250], [970, 90], [728, 90]],'bonsfluidos','970x250_top',[{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_970x250_top' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
-                { bidder: 'seedtag', params: {adUnitId: '29650077', publisherId: '3592-0609-01', placement: 'inBanner'}}
-            ]
-        }]],
-      [[[970, 250], [970, 90], [728, 90]], 'bonsfluidos', '970x250_middle', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_6' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
-                { bidder: 'seedtag', params: {adUnitId: '29651182', publisherId: '3592-0609-01', placement: 'inBanner'}}
-            ]
-        }]],
-   [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_right', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_1' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
-                { bidder: 'seedtag', params: {adUnitId: '21653362', publisherId: '3592-0609-01', placement: 'inArticle'}}
-            ]
-        }]],
-    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_1', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_2' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
-                { bidder: 'seedtag', params: {adUnitId: '21653364', publisherId: '3592-0609-01', placement: 'inArticle'}}
-            ]
-        }]],
-    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_2', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_3' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } },
-                { bidder: 'seedtag', params: {adUnitId: '21653365', publisherId: '3592-0609-01', placement: 'inArticle'}}
-            ]
-        }]],
-    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_3', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_4' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } }
-            ]
-        }]],
-    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_4', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x250_middle_5' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 186803, pageId: 171902 } }
-            ]
-        }]],
-    [[[250, 250], [300, 250]], 'bonsfluidos', '300x250_middle_5', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x600_right' } },
-                { bidder: 'appnexus', params: { placementId: 19274151 } },
-                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
-                { bidder: 'seedtag', params: {adUnitId: '29651185', publisherId: '3592-0609-01', placement: 'inBanner'}},
-            ]
-        }]],
-    [[[250, 250], [300, 600], [160, 600], [300, 250]], 'bonsfluidos', '300x600_right', [{
-            bids: [
-                { bidder: 'rubicon', params: { accountId: 10605, siteId: 55114, zoneId: 258958 } },
-                { bidder: 'pubmatic', params: { publisherId: '159456', adSlot: 'Bons_Fluidos_Desktop_300x600_right' } },
-                { bidder: 'appnexus', params: { placementId: 19027172 } },
-                { bidder: 'teads', params: { placementId: 131494, pageId: 121195 } }
-            ]
-        }]],
-        [[[728, 90], [970 , 90]],'bonsfluidos','anchor',[{
-             bids: [
-                { bidder: 'rubicon', params: {accountId: 10605, siteId: 322594, zoneId: 1712330 } },
-                { bidder: 'appnexus', params: {placementId: 19274151} },
-                { bidder: 'pubmatic', params: {publisherId: '159456', adSlot: 'Bons_Fluidos_Anchor_970x90'} },
-                { bidder: 'teads', params: { placementId: 131492, pageId: 121193 } },
-                { bidder: 'seedtag', params: {adUnitId: '21653363', publisherId: '3592-0609-01', placement: 'inScreen'}}
-      
-    ]
-  }]]
-      ]
+function carregaAD() {
+    console.log("Iniciando carregaAD...");
 
-      for(i = 0; i < googleFormats.length; i++){
-        console.log("-> " + googleFormats[i][2]).refresh();
-      }
-      
+    var googletag = window.googletag || { cmd: [] };
+    var pbjs = window.pbjs || { que: [] };
+
+    var divsCompletas = document.querySelectorAll('[id^="googleAd"]');
+    console.log("Número de divs encontradas:", divsCompletas.length);
+
+    var adUnits = [];
+    divsCompletas.forEach(function(div) {
+        var targetingValue = JSON.parse(div.getAttribute('data-targeting'));
+        console.log("Div ID:", div.id);
+        console.log("Targeting Value:", targetingValue);
+
+        var posValue = targetingValue[1][0];
+        var sizes = [[300, 250]];
+
+        if (div.id === "googleAd1" || posValue === "usFullsupH1") {
+            sizes = [[300, 250]];
+        } else if (div.id === "googleAd2" || posValue === "usFullsupH2") {
+            sizes = [[728, 90]];
+        } else if (div.id === "googleAd3" || posValue === "usFullsupH3") {
+            sizes = [[728, 90]];
+        }
+
+        adUnits.push({
+            code: div.id,
+            mediaTypes: {
+                banner: {
+                    sizes: sizes,
+                    path: "/7542/parceiros/BelezaDeMulher",
+                    pos: posValue
+                }
+            },
+            bids: [
+                { bidder: 'appnexus', params: { placementId: '-' } },
+                { bidder: 'rubicon', params: { accountId: 18966, siteId: 552008, zoneId: 3456976 } },
+                { bidder: 'smartadserver', params: { domain: '//prg.smartadserver.com', siteId: 669594, pageId: 2000293, formatId: 133313, currency: 'USD' } },
+                { bidder: 'sovrn', params: { tagid: '-' } }
+            ]
+        });
+    });
+
+    pbjs.que.push(function() {
+        pbjs.addAdUnits(adUnits);
+        pbjs.requestBids({
+            bidsBackHandler: function() {
+                divsCompletas.forEach(function(div) {
+                    var targetingValue = JSON.parse(div.getAttribute('data-targeting'));
+                    var posValue = targetingValue[1][0];
+
+                    var format = adUnits.find(function(format) {
+                        return format.mediaTypes.banner.pos === posValue && format.code === div.id;
+                    });
+
+                    if (format) {
+                        console.log("Formato encontrado:");
+                        console.log("-> ID: ", div.id);
+                        console.log("-> Posição: ", format.mediaTypes.banner.pos);
+
+                        googletag.cmd.push(function () {
+                            var slot = googletag
+                                .defineSlot(
+                                    format.mediaTypes.banner.path,
+                                    format.mediaTypes.banner.sizes,
+                                    div.id
+                                )
+                                .addService(googletag.pubads());
+
+                            googletag.enableServices();
+                            googletag.display(div.id);
+                            console.log("Anúncio exibido para:", div.id);
+                        });
+                    } else {
+                        console.log("Nenhum formato correspondente encontrado para", posValue);
+                    }
+                });
+
+                initAdServer();
+            }
+        });
+    });
+
+    function initAdServer() {
+        if (pbjs.initAdserverSet) return;
+        pbjs.initAdserverSet = true;
+        googletag.cmd.push(function() {
+            pbjs.setTargetingForGPTAsync();
+            googletag.pubads().refresh();
+        });
+    }
+
+    // Esperar pelo Prebid antes de habilitar os serviços GPT
+    pbjs.que.push(function() {
+        googletag.cmd.push(function () {
+            pbjs.setTargetingForGPTAsync();
+            googletag.enableServices();
+            console.log("GPT Services Enabled");
+        });
+    });
+
+    console.log("carregaAD finalizado.");
+}
+
+window.addEventListener('load', carregaAD);
